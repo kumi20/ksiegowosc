cfdca8995e35e9b8dd6bb4118620b5f59247a5c2
Jakub Kumięga <j.kumiega@icloud.com>
2019-05-23 18:18:33 +0200

    dodanie content

---
+++ src/app/book/content/content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ElementRef, Input, ViewChild, Output, EventEmitter, ViewChildren, AfterViewInit, OnDestroy} from '@angular/core';
+import { EventService } from '../../event.service';
+import { ApiService } from '../../api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent implements OnInit {
+
+  constructor(private CmsService: ApiService, private event: EventService, private route: ActivatedRoute, private _route: Router) { }
+
+  ngOnInit() {
+  }
+
+}
